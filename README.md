c101910a7ed78182d1c9115fdcc9b3f30306098c
fatwaabdusyukur <waengyoo@gmail.com>
2024-03-22 05:06:43 +0700

    Initiate Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# quick-news
+
+This is a chrome extension for searching news from the <a href="https://www.cnnindonesia.com">cnnindonesia.com</a> channel quickly and accurately, apart from providing news, this extension also provides recommendations and news summaries made by chatgpt.
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: {},
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "src/manifest.json", to: "manifest.json" },
+        { from: "src/assets/img/logo.png", to: "img/logo.png" },
+        { from: "src/assets/css/style.css", to: "style.css" },
+      ],
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    extensions: ["*", ".js", ".vue", ".json"],
+  },
+};
